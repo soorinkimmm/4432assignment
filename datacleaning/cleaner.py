27ed61ad00445c1c1b1081e5246b6825a18cabc1
김수린 <sulin@gimsulin-ui-MacBook-Air-2.local>
2016-03-06 20:17:45 -0600

    datacleaning assignment

---
+++ datacleaning/cleaner.py
@@ -0,0 +1,21 @@
+# Your code goes here
+import csv
+
+csvfile = open('./data/cleanme.csv', 'r')
+outfile = open('./data/clean.csv', 'w')
+
+reader = csv.DictReader(csvfile)
+writer = csv.DictWriter(outfile, reader.fieldnames)
+
+writer.writeheader()
+
+for row in reader:
+	row['first_name'] = row['first_name'].upper()
+	row['zip'] = row['zip'].zfill(5)
+	row['city'] = row['city'].replace('&nbsp;', '_')
+	if int(row['amount']) >= 1000:
+		writer.writerow(row)
+
+
+	
+
